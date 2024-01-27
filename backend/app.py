98a0539071b4ccd424ec28719dca133b7cc64d6a
Allison Zhao <allisonzhao.uni@gmail.com>
2024-01-27 18:10:25 -0500

    create api to get url of photos from albumID

--- backend/app.py
+++ backend/app.py
@@ -64,9 +64,41 @@ def accessImages():
         # Handle other exceptions
         return jsonify({'error': 'An error occurred'}), 500
 
-@app.route('/api/getAlbumItems', methods=['GET'])
+@app.route('/api/getAlbumItems', methods=['POST'])
 def albumItems():
-    return jsonify({'test': 'Test'}), 200
+    data = request.get_json()
+    token = data.get('token')
+    album_id = data.get('album_id')
+
+    if not token or not album_id:
+        return jsonify({'error': 'Token or album ID missing.'}), 400
+
+    google_photos_items_endpoint = f'https://photoslibrary.googleapis.com/v1/albums/{album_id}/mediaItems'
+
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-type': 'application/json'
+    }
+
+    try:
+        # Make a request to the Google Photos API to list the media items in the specified album
+        response = requests.get(google_photos_items_endpoint, headers=headers)
+        response.raise_for_status()
+
+        # Extract URLs of the photos from the response
+        items = response.json().get('mediaItems', [])
+        photo_urls = [item['baseUrl'] for item in items]
+
+        return jsonify({'photo_urls': photo_urls}), 200
+
+    except requests.RequestException as e:
+        # Handle request exceptions (like network errors)
+        return jsonify({'error': str(e)}), e.response.status_code
+
+    except Exception as e:
+        # Handle other exceptions
+        return jsonify({'error': 'An error occurred'}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
