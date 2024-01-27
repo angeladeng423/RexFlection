d06777230d9b611662c8a036f7de50be54fe5308
angela <adeng32@uwo.ca>
2024-01-26 23:26:46 -0500

    Updated backend.
    Co-authored-by: Allison Zhao <allison-stack@users.noreply.github.com>

--- backend/app.py
+++ backend/app.py
@@ -12,21 +12,9 @@ import cohere
 # if __name__ == '__main__':
 #     app.run(debug=True)
 
-co = cohere.Client('YgLklAre1AZKzH8KUoFxPdi5JWohR2mkI6cN0a9Q')
-
+import cohere
+co = cohere.Client('YgLklAre1AZKzH8KUoFxPdi5JWohR2mkI6cN0a9Q') # This is your trial API key
 response = co.generate(
-  prompt='Please explain to me how LLMs work',
-)
-print(response)
-
-
-response1 = co.chat(
-  chat_history=[
-    {"role": "USER", "message": "Who discovered gravity?"},
-    {"role": "CHATBOT", "message": "The man who is widely credited with discovering gravity is Sir Isaac Newton"}
-  ],
-  message="What year was he born?",
-  # perform web search before answering the question. You can also use your own custom connector.
-  connectors=[{"id": "web-search"}]
-)
-print(response1)
+  model='e6bce515-b238-4601-af86-a3d61091288e-ft',
+  prompt='bubble tea, phone, laptop, keyboard')
+print('Prediction: {}'.format(response.generations[0].text))
